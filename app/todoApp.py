d3bffa4ec9fee27623c4aa15c50687afbabef2c8
5yndr0m <sdilanjana21@gmail.com>
2024-06-12 23:33:03 +0530

    function to remove todos added

--- app/todoApp.py
+++ app/todoApp.py
@@ -48,3 +48,20 @@ class Todoer:
         todo["Done"] = True
         write = self._db_handler.write_todos(read.todo_list)
         return CurrentToDo(todo, write.error)
+
+    def remove(self, todo_id: int) -> CurrentToDo:
+        """Remove a to-do from the database using its id or index."""
+        read = self._db_handler.read_todos()
+        if read.error:
+            return CurrentToDo({}, read.error)
+        try:
+            todo = read.todo_list.pop(todo_id - 1)
+        except IndexError:
+            return CurrentToDo({}, ID_ERROR)
+        write = self._db_handler.write_todos(read.todo_list)
+        return CurrentToDo(todo, write.error)
+
+    def remove_all(self) -> CurrentToDo:
+        """Remove all to-dos from the database."""
+        write = self._db_handler.write_todos([])
+        return CurrentToDo({}, write.error)
